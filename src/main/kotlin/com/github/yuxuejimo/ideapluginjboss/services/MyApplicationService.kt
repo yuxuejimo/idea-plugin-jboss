42121bcc5d2ce7f5723b3b12131cac9aadd49278
GitHub Action <action@github.com>
2020-09-17 05:53:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuxuejimo/ideapluginjboss/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yuxuejimo/ideapluginjboss/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yuxuejimo.ideapluginjboss.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yuxuejimo.ideapluginjboss.MyBundle
 
 class MyApplicationService {
 
